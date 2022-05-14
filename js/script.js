3766dc432e9cbbd1fa90ce43f34bf497759977e3
MoaazAbdalla <103704192+MoaazAbdalla@users.noreply.github.com>
2022-05-14 10:25:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $("#temizle").click(function(){
+        $("#adi").val("");
+        $("#soyad").val("");
+        $("#email").val("");
+        $("#mesaj").val("");
+
+        });
+});
